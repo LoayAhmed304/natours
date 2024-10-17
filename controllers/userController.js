de58e3dff6c6302cbd64535bdff0e53f46dae9ff
Loay Ahmed <loayahmed304@gmail.com>
2024-10-17 23:50:08 +0300

    feat: add user /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,6 +24,11 @@ const filterObj = (obj, ...allowedFields) => {
 //   });
 // });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
