71fb7da3c1b5869a1a7050460f006f0ae2040919
Loay Ahmed <loayahmed304@gmail.com>
2024-10-18 11:11:59 +0300

    feat: get nearest tours within a given location and radius

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,6 +120,7 @@ const tourSchema = new mongoose.Schema(
 // tourSchema.index({ price: 1 }); // sorting ascendingly, to make find faster
 tourSchema.index({ price: 1, ratingsAverage: -1 }); // sorting ascendingly - descendingly
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
